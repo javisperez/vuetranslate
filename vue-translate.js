3dff416cae00bcd1a1cfa83cee21bdcdc7bfaab3
Javis V. Pérez <javisperez@gmail.com>
2016-08-24 11:29:25 -0400

    package.json and dist folder

--- vue-translate.js
+++ vue-translate.js
@@ -8,102 +8,99 @@
  * @author Javis Perez <javisperez@gmail.com>
  * https://github.com/javisperez/vuetranslate
  */
-;(function () {
 
-    // We need a vue instance to handle rectivity
-    var vm = null;
+// We need a vue instance to handle rectivity
+var vm = null;
 
-    /**
-     * Constructor
-     */
-    function VueTranslate() {}
+/**
+ * Constructor
+ */
+function VueTranslate() {}
 
-    // Install the method
-    VueTranslate.install = function (Vue) {
-        if (!vm) {
-            vm = new Vue({
-                data() {
-                    return {
-                        current: '',
-                        locales: {}
-                    };
-                },
+// Install the method
+VueTranslate.install = function (Vue) {
+    if (!vm) {
+        vm = new Vue({
+            data() {
+                return {
+                    current: '',
+                    locales: {}
+                };
+            },
 
-                computed: {
-                    locale() {
-                        if (!this.locales[this.current])
-                            return null;
+            computed: {
+                locale() {
+                    if (!this.locales[this.current])
+                        return null;
 
-                        return this.locales[this.current];
-                    }
-                },
-
-                methods: {
-                    setLang(val) {
-                        this.current = val;
-                    },
-
-                    setLocales(locales) {
-                        if (!locales)
-                            return;
-
-                        let newLocale = Object.create(this.locales);
-
-                        for (let key in locales) {
-                            if (!newLocale[key])
-                                newLocale[key] = {};
-
-                            Vue.util.extend(newLocale[key], locales[key]);
-                        }
-
-                        this.locales = Object.create(newLocale);
-                    },
-
-                    text(t) {
-                        if (!this.locale || !this.locale[t]) {
-                            return t;
-                        }
-
-                        return this.locale[t];
-                    }
+                    return this.locales[this.current];
                 }
-            });
-
-            Vue.prototype.$translate = vm;
-        }
-
-        // Mixin to read locales and add the translation method and directive
-        Vue.mixin({
-            beforeCreate() {
-                this.$translate.setLocales(this.$options.locales);
             },
 
             methods: {
-                t(t) {
-                    return this.$translate.text(t);
+                setLang(val) {
+                    this.current = val;
+                },
+
+                setLocales(locales) {
+                    if (!locales)
+                        return;
+
+                    let newLocale = Object.create(this.locales);
+
+                    for (let key in locales) {
+                        if (!newLocale[key])
+                            newLocale[key] = {};
+
+                        Vue.util.extend(newLocale[key], locales[key]);
+                    }
+
+                    this.locales = Object.create(newLocale);
+                },
+
+                text(t) {
+                    if (!this.locale || !this.locale[t]) {
+                        return t;
+                    }
+
+                    return this.locale[t];
                 }
-            },
-
-            directives: {
-                translate: function (el) {
-                    if (!el.$translateKey)
-                        el.$translateKey = el.innerText;
-
-                    let text = this.$translate.text(el.$translateKey);
-
-                    el.innerText = text;
-                }.bind(vm)
             }
         });
-    };
 
-    if (typeof exports === 'object') {
-        module.exports = VueTranslate; // CommonJS
-    } else if (typeof define === 'function' && define.amd) {
-        define([], function () { return VueTranslate; }); // AMD
-    } else if (window.Vue) {
-        window.VueTranslate = VueTranslate; // Browser (not required options)
-        Vue.use(VueTranslate);
+        Vue.prototype.$translate = vm;
     }
 
-})();
+    // Mixin to read locales and add the translation method and directive
+    Vue.mixin({
+        beforeCreate() {
+            this.$translate.setLocales(this.$options.locales);
+        },
+
+        methods: {
+            t(t) {
+                return this.$translate.text(t);
+            }
+        },
+
+        directives: {
+            translate: function (el) {
+                if (!el.$translateKey)
+                    el.$translateKey = el.innerText;
+
+                let text = this.$translate.text(el.$translateKey);
+
+                el.innerText = text;
+            }.bind(vm)
+        }
+    });
+};
+
+if (typeof exports === 'object') {
+    module.exports = VueTranslate; // CommonJS
+} else if (typeof define === 'function' && define.amd) {
+    define([], function () { return VueTranslate; }); // AMD
+} else if (window.Vue) {
+    window.VueTranslate = VueTranslate; // Browser (not required options)
+    Vue.use(VueTranslate);
+}
